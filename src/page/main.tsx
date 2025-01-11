5e553bea790e48546acee0571197e64f21c023d7
Joy Kim <feel5.nayoung@gmail.com>
2025-01-11 16:13:03 +0900

    feat: 메인페이지 진입시 검색 섹션이 노출되어있다.-추가기능-구현

--- src/page/main.tsx
+++ src/page/main.tsx
@@ -1,11 +1,9 @@
-import { useGetListNowPlayingMovie } from '../api/get-list-now-playing-movie';
-
 export const MovieMain = () => {
   return (
     <>
       <section data-testid="search-section">
         <h1 data-testid="search-title">영화 List</h1>
-        <input data-testid="search-input" />
+        <input data-testid="search-input" placeholder="영화를 검색해주세요" />
         <button data-testid="search-button">검색</button>
       </section>
     </>
