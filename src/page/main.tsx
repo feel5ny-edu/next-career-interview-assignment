368ac81635c89e44e5620032fba4a3367bcf55ad
Joy Kim <feel5.nayoung@gmail.com>
2025-01-11 16:08:22 +0900

    feat: 메인페이지 진입시 검색 섹션이 노출되어있다.-통과구현

--- src/page/main.tsx
+++ src/page/main.tsx
@@ -1,3 +1,13 @@
+import { useGetListNowPlayingMovie } from '../api/get-list-now-playing-movie';
+
 export const MovieMain = () => {
-  return <>MovieMain</>;
+  return (
+    <>
+      <section data-testid="search-section">
+        <h1 data-testid="search-title">영화 List</h1>
+        <input data-testid="search-input" />
+        <button data-testid="search-button">검색</button>
+      </section>
+    </>
+  );
 };
