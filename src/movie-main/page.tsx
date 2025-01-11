16e3aa7d5691d7a7e7fdd873571e721c90cef4d5
Joy Kim <feel5.nayoung@gmail.com>
2025-01-11 22:19:16 +0900

    refactor: movie result list 분리

--- src/movie-main/page.tsx
+++ src/movie-main/page.tsx
@@ -1,27 +1,30 @@
 import { useState } from 'react';
-import { useSearchMovie } from '../api/get-search-movie';
+import {
+  ResponseGetSearchMovie,
+  useSearchMovie,
+} from '../api/get-search-movie';
 import { MovieSearchSection } from './components/movie-search-section';
 import { NowPlayingMovieList } from './components/now-playing-movie-list';
 import { MovieSearchResultList } from './components/movie-search-result-list';
-import { useGetListNowPlayingMovie } from '../api/get-list-now-playing-movie';
+import {
+  ResponseGetListNowPlayingMovie,
+  useGetListNowPlayingMovie,
+} from '../api/get-list-now-playing-movie';
 
 export const MovieMain = () => {
   const [searchKeyword, setSearchKeyword] = useState('');
 
-  const {
-    data: searchMovieList,
-    isFetched: isFetchedSearchMovieList,
-    isLoading: isLoadingSearchMovieList,
-  } = useSearchMovie(
-    {
-      query: searchKeyword,
-      page: 1,
-    },
-    { enabled: searchKeyword.length > 0 }
-  );
+  const { data: searchMovieList, isLoading: isLoadingSearchMovieList } =
+    useSearchMovie(
+      {
+        query: searchKeyword,
+        page: 1,
+      },
+      { enabled: searchKeyword.length > 0 }
+    );
+
   const {
     data: listNowPlayingMovie,
-    isFetched: isFetchedGetListNowPlayingMovie,
     isLoading: isLoadingGetListNowPlayingMovie,
   } = useGetListNowPlayingMovie({
     page: 1,
@@ -31,17 +34,27 @@ export const MovieMain = () => {
     <>
       <MovieSearchSection setSearchKeyword={setSearchKeyword} />
 
-      {/* 목록 섹션 */}
-      {(isLoadingSearchMovieList || isLoadingGetListNowPlayingMovie) && (
-        <>로딩중</>
-      )}
-
-      {isFetchedGetListNowPlayingMovie && (
-        <NowPlayingMovieList movieList={listNowPlayingMovie} />
-      )}
-      {isFetchedSearchMovieList && (
-        <MovieSearchResultList movieList={searchMovieList} />
-      )}
+      <MovieListSection
+        isLoading={isLoadingSearchMovieList || isLoadingGetListNowPlayingMovie}
+        searchMovieList={searchMovieList}
+        listNowPlayingMovie={listNowPlayingMovie}
+      />
     </>
   );
+};
+
+const MovieListSection = ({
+  isLoading,
+  searchMovieList,
+  listNowPlayingMovie,
+}: {
+  isLoading: boolean;
+  searchMovieList?: ResponseGetSearchMovie;
+  listNowPlayingMovie?: ResponseGetListNowPlayingMovie;
+}) => {
+  if (isLoading) return <>로딩중</>;
+  if (searchMovieList)
+    return <MovieSearchResultList movieList={searchMovieList} />;
+  if (listNowPlayingMovie)
+    return <NowPlayingMovieList movieList={listNowPlayingMovie} />;
 };
