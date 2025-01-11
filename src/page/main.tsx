8fce351c459e7ed40f0f16c9a50b550c8ca4df77
Joy Kim <feel5.nayoung@gmail.com>
2025-01-11 16:30:55 +0900

    feat: 검색어를 한글자 이상 입력해야, 검색버튼이 활성화된다.-구현

--- src/page/main.tsx
+++ src/page/main.tsx
@@ -1,10 +1,22 @@
+import { ChangeEvent, useState } from 'react';
+
 export const MovieMain = () => {
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const handleSearchInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchKeyword(e.target.value);
+  };
   return (
     <>
       <section data-testid="search-section">
         <h1 data-testid="search-title">영화 List</h1>
-        <input data-testid="search-input" placeholder="영화를 검색해주세요" />
-        <button data-testid="search-button">검색</button>
+        <input
+          data-testid="search-input"
+          placeholder="영화를 검색해주세요"
+          onChange={handleSearchInput}
+        />
+        <button data-testid="search-button" disabled={!Boolean(searchKeyword)}>
+          검색
+        </button>
       </section>
       <section data-testid="now-playing-section">
         <h2 data-testid="now-playing-title">현재 상영중인 영화</h2>
