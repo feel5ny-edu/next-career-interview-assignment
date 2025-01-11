e86e4110efa8aed9f88dcb5860f2fd79579c0106
Joy Kim <feel5.nayoung@gmail.com>
2025-01-11 21:42:54 +0900

    refactor: 검색섹션 분리

---
+++ src/movie-main/components/movie-search-section.tsx
@@ -0,0 +1,38 @@
+import { ChangeEvent, useRef, useState } from 'react';
+
+export const MovieSearchSection = ({
+  setSearchKeyword,
+}: {
+  setSearchKeyword: (inputData: string) => void;
+}) => {
+  const inputData = useRef('');
+  const [isDisabledButton, setIsDisabledButton] = useState(true);
+
+  const handleSearchInput = (e: ChangeEvent<HTMLInputElement>) => {
+    inputData.current = e.target.value;
+    if (e.target.value) setIsDisabledButton(false);
+    else setIsDisabledButton(true);
+  };
+
+  const handleSearchButton = () => {
+    setSearchKeyword(inputData.current);
+  };
+
+  return (
+    <section data-testid="search-section">
+      <h1 data-testid="search-title">영화 List</h1>
+      <input
+        data-testid="search-input"
+        placeholder="영화를 검색해주세요"
+        onChange={handleSearchInput}
+      />
+      <button
+        data-testid="search-button"
+        disabled={isDisabledButton}
+        onClick={handleSearchButton}
+      >
+        검색
+      </button>
+    </section>
+  );
+};
--- src/movie-main/page.tsx
+++ src/movie-main/page.tsx
@@ -1,10 +1,9 @@
-import { ChangeEvent, useRef, useState } from 'react';
+import { useState } from 'react';
 import { useGetSearchMovie } from '../api/get-search-movie';
+import { MovieSearchSection } from './components/movie-search-section';
 
 export const MovieMain = () => {
-  const inputData = useRef('');
   const [searchKeyword, setSearchKeyword] = useState('');
-  const [isDisabledButton, setIsDisabledButton] = useState(true);
 
   const { data, isSuccess, isLoading } = useGetSearchMovie(
     {
@@ -14,33 +13,10 @@ export const MovieMain = () => {
     { enabled: searchKeyword.length > 0 }
   );
 
-  const handleSearchInput = (e: ChangeEvent<HTMLInputElement>) => {
-    inputData.current = e.target.value;
-    if (e.target.value) setIsDisabledButton(false);
-    else setIsDisabledButton(true);
-  };
-
-  const handleSearchButton = () => {
-    setSearchKeyword(inputData.current);
-  };
-
   return (
     <>
-      <section data-testid="search-section">
-        <h1 data-testid="search-title">영화 List</h1>
-        <input
-          data-testid="search-input"
-          placeholder="영화를 검색해주세요"
-          onChange={handleSearchInput}
-        />
-        <button
-          data-testid="search-button"
-          disabled={isDisabledButton}
-          onClick={handleSearchButton}
-        >
-          검색
-        </button>
-      </section>
+      <MovieSearchSection setSearchKeyword={setSearchKeyword} />
+
       {!isSuccess && !isLoading && (
         <section data-testid="now-playing-section">
           <h2 data-testid="now-playing-title">현재 상영중인 영화</h2>
