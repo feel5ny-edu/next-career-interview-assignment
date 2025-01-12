e4cf212aaf7bb1f3935b471be1cb4ee12c1ee7c5
Joy Kim <feel5.nayoung@gmail.com>
2025-01-12 13:55:13 +0900

    style: detail 페이지 스타일작업

--- src/movie-detail/page.tsx
+++ src/movie-detail/page.tsx
@@ -12,14 +12,19 @@ export const MovieDetail = () => {
   if (!data) return null;
 
   return (
-    <>
-      <h1 data-testid="movie-title">{data.title}</h1>
+    <div className="text-center">
+      <h1 data-testid="movie-title" className="text-4xl p-8">
+        {data.title}
+      </h1>
       <img
         data-testid="movie-poster"
         src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
+        className="rounded-lg"
       />
-      <div data-testid="movie-vote">{data.vote_average}</div>
+      <div data-testid="movie-vote" className="text-xl py-4">
+        평점 {data.vote_average}
+      </div>
       <div data-testid="movie-comment">{COMMENT}</div>
-    </>
+    </div>
   );
 };
